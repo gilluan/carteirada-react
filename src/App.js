a8088a86633af578adba87a383e800497132cf3e
gilluan.formiga <gilluan@hotmail.com.br>
2017-02-03 17:27:42 -0200

    Inclusão de rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import NavBar from './stories/NavBar';
+import { ButtonToolbar, Button } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large" active>Primary button</Button>
+          <Button bsSize="large" active>Button</Button>
+        </ButtonToolbar>
       </div>
     );
   }
